5f209dddd14839db09ea330851899e965514793d
loyol <loyoladesa@gmail.com>
2024-09-26 12:05:44 -0300

    coleta3

--- client_vlc/download_dados_cluster.py
+++ client_vlc/download_dados_cluster.py
@@ -9,8 +9,8 @@ data_formatada = data_atual.strftime("%d%m%Y")
 
 
 #os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_rn_" + data_formatada + ".csv qoe_value_rn_" + data_formatada + ".csv")
-#os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_ac_" + data_formatada + ".csv qoe_value_ac_" + data_formatada + ".csv")
+os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_ac_" + data_formatada + ".csv qoe_value_ac_" + data_formatada + ".csv")
 os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_go_" + data_formatada + ".csv qoe_value_go_" + data_formatada + ".csv")
-#os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_mt_" + data_formatada + ".csv qoe_value_mt_" + data_formatada + ".csv")
+os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_mt_" + data_formatada + ".csv qoe_value_mt_" + data_formatada + ".csv")
 #os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_pb_" + data_formatada + ".csv qoe_value_pb_" + data_formatada + ".csv")
-#os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_pe_" + data_formatada + ".csv qoe_value_pe_" + data_formatada + ".csv")
+os.system("scp -P 2200 qoeprediction@200.130.25.172:/home/qoeprediction/qoe_value_pe_" + data_formatada + ".csv qoe_value_pe_" + data_formatada + ".csv")
